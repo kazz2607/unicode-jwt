136efa30fcf496c56328a121bc4bd767e755f134
kazz2607 <kairu2607@gmail.com>
2024-11-30 14:45:47 +0700

    Bai 144

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,50 +2,86 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login','refresh']]);
     }
 
     public function login()
     {
         $credentials = request(['email', 'password']);
-
         if (! $token = auth('api')->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
-
-        return $this->respondWithToken($token);
+        $refreshToken = $this->createRefreshToken();
+        return $this->respondWithToken($token,$refreshToken);
     }
+
+   
 
     public function profile()
     {
-        return response()->json(auth('api')->user());
+        try{
+            return response()->json(auth('api')->user());
+        }catch(JWTException $exception){
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
     }
 
     public function logout()
     {
         auth('api')->logout();
-
         return response()->json(['message' => 'Successfully logged out']);
     }
 
-    public function refresh()
+    public function refresh(Request $request)
     {
-        return $this->respondWithToken(auth('api')->refresh());
+        $refreshToken = $request->refresh_token;
+        try{
+            $decode = JWTAuth::getJWTProvider()->decode($refreshToken);
+            /** Xử lý cấp lại Token mới
+             * => Lấy thông tin user
+             * 
+            */
+            $user = User::find($decode['user_id']);
+            if (!$user){
+                return response()->json(['error' => 'User Not Found'], 404);
+            }
+            $token = auth('api')->login($user);
+            $refreshToken = $this->createRefreshToken();
+            return $this->respondWithToken($token,$refreshToken);
+
+        }catch(JWTException $exception){
+            return response()->json(['error' => 'Refresh Token Invalid'], 500);
+        }
     }
 
-    protected function respondWithToken($token)
+    private function respondWithToken($token, $refreshToken)
     {
         return response()->json([
             'access_token' => $token,
+            'refresh_token' => $refreshToken,
             'token_type' => 'bearer',
             'expires_in' => auth('api')->factory()->getTTL() * 60
         ]);
+    }
+
+    private function createRefreshToken(){
+        $data = [
+            'user_id' => auth('api')->user()->id,
+            'radom' => rand().time(),
+            'exp' => time() + config('jwt.refresh_ttl')
+        ];
+        $refreshToken = JWTAuth::getJWTProvider()->encode($data);
+        return $refreshToken;
     }
 }
--- note.md
+++ note.md
@@ -8,5 +8,13 @@ Request get Data : Gửi token thông qua Header (Authorization : Bearer <token>
 
 Server -> Kiểm tra token có hợp lệ hay không ? -> Decode Payload -> Truy vấn Database trả về dữ liệu
 
+## Bảo mật Token
 
+AccessToken : Nếu bị đánh cắp => Hacker khai thác dựa vào token
+-> Giải pháp : Hạ thấp thời gian sống của AccessToken -> Gây phiền phức cho người dùng
+-> Cần bổ sung : RefreshToken -> Thời gian sống lâu hơn -> Dùng cấp lại AccessToken mới sau khi AccessToken cũ hết hạn
+-> Khi logout -> Thêm token và Backlist -> Khi Authorization -> Cần kiểm tra token có trong backlist không
+    + Tính hợp lệ
+    + Thời gian sống
+    + Có trong blacklist hay không
 
